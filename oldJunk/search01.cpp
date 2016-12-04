9d64b81553a824f1c589660116cf19a575d8eb74
ordogfioka <megyesi.attika@gmail.com>
2016-12-04 14:17:16 +0100

    4 threads

--- oldJunk/search01.cpp
+++ oldJunk/search01.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-#include <ctime>
+#include <atomic>
+#include <thread>
 
 #define M 6
 #define N 1
 #define NSTATES 24*24*24*24*24*24 // Should be 24^(N*M)
+#define THREADS = 1;
 
 using namespace std;
 
@@ -18,7 +20,7 @@ int Right[24] = {13, 18, 5, 15, 2, 7, 9, 14, -7, 11, -10, 3, -3, 10, -11, 7, -14
 
 int Pow24[7] = {1, 24, 576, 13824, 331776, 7962624, 191102976}; //, 4586471424L, 110075314176L, 2641807540224L;
 
-void n2tuple(int k, vector<int>& ret)
+void n2myTuple(int k, vector<int>& ret)
 {
 	for (int i = 0; i < M*N; ++i) 
 	{
@@ -26,91 +28,108 @@ void n2tuple(int k, vector<int>& ret)
 		k = k / 24;
 	}
 }
-std::chrono::time_point<std::chrono::system_clock> start, end;
+
+vector<bool> known(NSTATES, false);
+
+vector<bool> newKnown(NSTATES, false);
+
+int nKnown = 0;
+std::atomic<int> nNewKnown;
+int level = 0;
+vector<int> myTuple(M*N);
+
+void doit(int start, int finish)
+{
+	int neighb,myNewKnown=0;
+	for(int curr=start;curr<finish;++curr)
+	{
+		// For all known points
+		if (known[curr]) 
+		{
+			// Convert to myTuple
+			n2myTuple(curr, myTuple);
+
+			// For all its neighbours, make them known, too.
+			// For each row
+			for (int j = 0; j < M; ++j) 
+			{
+				// Up neighbour
+				neighb = curr;
+				for (int k = j*N; k < (j+1)*N; ++k) 
+				{
+					neighb += Up[myTuple[k]] * Pow24[k];
+				}
+				if (!newKnown[neighb]) 
+				{
+					++myNewKnown; 
+					newKnown[neighb] = true;
+				}
+				// Down neighbour
+				neighb = curr;
+				for (int k = j*N; k < (j+1)*N; ++k) 
+				{
+					neighb += Down[myTuple[k]] * Pow24[k];
+				}
+				if (!newKnown[neighb]) 
+				{
+					++myNewKnown; 
+					newKnown[neighb] = true;
+				}
+			}
+			// For each column
+			for (int j = 0; j < N; ++j) 
+			{
+				// Left neighbour
+				neighb = curr;
+				for (int k = j; k < M*N; k += N) 
+				{
+					neighb += Left[myTuple[k]] * Pow24[k];
+				}
+				if (!newKnown[neighb]) 
+				{
+					++myNewKnown; 
+					newKnown[neighb] = true;
+				}
+				// Right neighbour
+				neighb = curr;
+				for (int k = j; k < M*N; k += N) 
+				{
+					neighb += Right[myTuple[k]] * Pow24[k];
+				}
+				if (!newKnown[neighb]) 
+				{
+					++myNewKnown; 
+					newKnown[neighb] = true;
+				}
+			}
+		}
+	}
+	nNewKnown += myNewKnown;
+}
+
+std::vector<std::thread> threads;
 int main()
 {
-	start = std::chrono::system_clock::now();
-	vector<bool> known(NSTATES, false);
 	known[0] = true;
-
-	vector<bool> newKnown(NSTATES, false);
 	newKnown[0] = true;
-
-	int nKnown = 0;
-	int nNewKnown = 1;
-	int level = 0;
-
-	vector<int> tuple(M*N);
+	nNewKnown = 1;
+	auto start = std::chrono::steady_clock::now();
 
 	while (nKnown != nNewKnown) 
 	{
 		cout << "There are " << (nNewKnown - nKnown) << " states on level " << level << " .\n";
 		++level;
 		nKnown = nNewKnown;
-		int curr, j, k, neighb;
-		for (curr = 0; curr < NSTATES; ++curr) 
-		{
-			// For all known points
-			if (known[curr]) 
-			{
-				// Convert to tuple
-				n2tuple(curr, tuple);
-
-				// For all its neighbours, make them known, too.
-				// For each row
-				for (j = 0; j < M; ++j) 
-				{
-					// Up neighbour
-					neighb = curr;
-					for (k = j*N; k < (j+1)*N; ++k) 
-					{
-						neighb += Up[tuple[k]] * Pow24[k];
-					}
-					if (!newKnown[neighb]) 
-					{
-						++nNewKnown; 
-						newKnown[neighb] = true;
-					}
-					// Down neighbour
-					neighb = curr;
-					for (k = j*N; k < (j+1)*N; ++k) 
-					{
-						neighb += Down[tuple[k]] * Pow24[k];
-					}
-					if (!newKnown[neighb]) 
-					{
-						++nNewKnown; 
-						newKnown[neighb] = true;
-					}
-				}
-				// For each column
-				for (j = 0; j < N; ++j) 
-				{
-					// Left neighbour
-					neighb = curr;
-					for (k = j; k < M*N; k += N) 
-					{
-						neighb += Left[tuple[k]] * Pow24[k];
-					}
-					if (!newKnown[neighb]) 
-					{
-						++nNewKnown; 
-						newKnown[neighb] = true;
-					}
-					// Right neighbour
-					neighb = curr;
-					for (k = j; k < M*N; k += N) 
-					{
-						neighb += Right[tuple[k]] * Pow24[k];
-					}
-					if (!newKnown[neighb]) 
-					{
-						++nNewKnown; 
-						newKnown[neighb] = true;
-					}
-				}
-			}
-		}
+		
+		std::thread t1 (doit, 0, NSTATES/4);
+		t1.join();
+		std::thread t2 (doit, NSTATES/4, 2*(NSTATES/4));
+		t2.join();
+		std::thread t3 (doit, 2*(NSTATES/4), 3*(NSTATES/4));
+		t3.join();
+		std::thread t4 (doit, 3*(NSTATES/4), NSTATES);
+		t4.join();
+		
 		for (int i = 0; i < NSTATES; ++i) 
 		{
 			if (newKnown[i] && !known[i]) 
@@ -119,7 +138,9 @@ int main()
 			}
 		}
 		cout << endl;
-
 	}//while
+	auto end = std::chrono::steady_clock::now();
 	cout << "Altogether " << nKnown << " states reached.\n";
+	std::chrono::duration<double> diff = end-start;
+    std::cout << "Time required:" << diff.count() << " s\n";
 }
