247a31e8afb921d0605f232ba6cc1d92ecda8934
Attila MEGYESI <attila.megyesi@nng.com>
2016-12-04 17:15:29 +0100

    std::thread

--- oldJunk/search01.cpp
+++ oldJunk/search01.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <atomic>
 #include <thread>
+#include <atomic>
 
 #define M 6
 #define N 1
 #define NSTATES 24*24*24*24*24*24 // Should be 24^(N*M)
-#define THREADS = 1;
 
 using namespace std;
 
@@ -20,6 +18,12 @@ int Right[24] = {13, 18, 5, 15, 2, 7, 9, 14, -7, 11, -10, 3, -3, 10, -11, 7, -14
 
 int Pow24[7] = {1, 24, 576, 13824, 331776, 7962624, 191102976}; //, 4586471424L, 110075314176L, 2641807540224L;
 
+vector<bool> known(NSTATES, false);
+vector<bool> newKnown(NSTATES, false);
+vector<int> myTuple(M*N);
+atomic<int> nNewKnown;
+int THREADS = 3;
+
 void n2myTuple(int k, vector<int>& ret)
 {
 	for (int i = 0; i < M*N; ++i) 
@@ -29,19 +33,11 @@ void n2myTuple(int k, vector<int>& ret)
 	}
 }
 
-vector<bool> known(NSTATES, false);
-
-vector<bool> newKnown(NSTATES, false);
-
-int nKnown = 0;
-std::atomic<int> nNewKnown;
-int level = 0;
-vector<int> myTuple(M*N);
-
-void doit(int start, int finish)
+void doit(int start,int end)
 {
-	int neighb,myNewKnown=0;
-	for(int curr=start;curr<finish;++curr)
+	int myNewKnown = 0;
+	int curr, j, k, neighb;
+	for (curr = 0; curr < NSTATES; ++curr) 
 	{
 		// For all known points
 		if (known[curr]) 
@@ -51,11 +47,11 @@ void doit(int start, int finish)
 
 			// For all its neighbours, make them known, too.
 			// For each row
-			for (int j = 0; j < M; ++j) 
+			for (j = 0; j < M; ++j) 
 			{
 				// Up neighbour
 				neighb = curr;
-				for (int k = j*N; k < (j+1)*N; ++k) 
+				for (k = j*N; k < (j+1)*N; ++k) 
 				{
 					neighb += Up[myTuple[k]] * Pow24[k];
 				}
@@ -66,7 +62,7 @@ void doit(int start, int finish)
 				}
 				// Down neighbour
 				neighb = curr;
-				for (int k = j*N; k < (j+1)*N; ++k) 
+				for (k = j*N; k < (j+1)*N; ++k) 
 				{
 					neighb += Down[myTuple[k]] * Pow24[k];
 				}
@@ -77,11 +73,11 @@ void doit(int start, int finish)
 				}
 			}
 			// For each column
-			for (int j = 0; j < N; ++j) 
+			for (j = 0; j < N; ++j) 
 			{
 				// Left neighbour
 				neighb = curr;
-				for (int k = j; k < M*N; k += N) 
+				for (k = j; k < M*N; k += N) 
 				{
 					neighb += Left[myTuple[k]] * Pow24[k];
 				}
@@ -92,7 +88,7 @@ void doit(int start, int finish)
 				}
 				// Right neighbour
 				neighb = curr;
-				for (int k = j; k < M*N; k += N) 
+				for (k = j; k < M*N; k += N) 
 				{
 					neighb += Right[myTuple[k]] * Pow24[k];
 				}
@@ -107,28 +103,34 @@ void doit(int start, int finish)
 	nNewKnown += myNewKnown;
 }
 
-std::vector<std::thread> threads;
 int main()
 {
 	known[0] = true;
 	newKnown[0] = true;
-	nNewKnown = 1;
-	auto start = std::chrono::steady_clock::now();
 
+	int nKnown = 0;
+	nNewKnown = 1;
+	int level = 0;
+	auto start = std::chrono::steady_clock::now();
+	
 	while (nKnown != nNewKnown) 
 	{
 		cout << "There are " << (nNewKnown - nKnown) << " states on level " << level << " .\n";
 		++level;
 		nKnown = nNewKnown;
 		
-		std::thread t1 (doit, 0, NSTATES/4);
-		t1.join();
-		std::thread t2 (doit, NSTATES/4, 2*(NSTATES/4));
-		t2.join();
-		std::thread t3 (doit, 2*(NSTATES/4), 3*(NSTATES/4));
-		t3.join();
-		std::thread t4 (doit, 3*(NSTATES/4), NSTATES);
-		t4.join();
+		//ide a parhuzamositast
+		std::vector<std::thread> results;
+		for (size_t i = 0; i < THREADS-1; i++)
+		{
+			results.push_back(thread(doit,i*(NSTATES/THREADS),(i+1)*(NSTATES/THREADS) ));
+		}
+		results.push_back(thread(doit,(NSTATES-1)*(NSTATES/THREADS),NSTATES ));
+
+		for (size_t i = 0; i < THREADS; i++)
+		{
+			results[i].join();
+		}
 		
 		for (int i = 0; i < NSTATES; ++i) 
 		{
@@ -138,7 +140,8 @@ int main()
 			}
 		}
 		cout << endl;
-	}//while
+
+	}
 	auto end = std::chrono::steady_clock::now();
 	cout << "Altogether " << nKnown << " states reached.\n";
 	std::chrono::duration<double> diff = end-start;
