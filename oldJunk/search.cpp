0750e7a2d031471773d36b68a922881931cbc414
dwat3r <hechtloliver@gmail.com>
2016-12-14 21:43:42 +0100

    init

--- oldJunk/search.cpp
+++ oldJunk/search.cpp
@@ -1,102 +1,101 @@
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <set>
-#include <ext/hash_set>
+#include <unordered_set>
 
 #define M 6
 #define N 1
 #define NSTATES 24*24*24*24*24*24 // Should be 24^(N*M)
 
-using namespace std;
-using namespace __gnu_cxx;
-
 // Global arrays
-int Up[24] = {22, 15, 18, 7, 10, 3, 17, 10, 10, -5, 2, -9, 9, -2, 5, -10, -10, -17, -3, -10, -7, -18, -15, -22};
-int Down[24] = {17, 22, 9, 18, 5, 10, 10, 15, -3, 10, -7, 2, -2, 7, -10, 3, -15, -10, -10, -5, -18, -9, -22, -17};
-int Left[24] = {10, 7, 14, 11, 18, 15, -2, -5, 9, 3, 13, 7, -7, -13, -3, -9, 5, 2, -15, -18, -11, -14, -7, -10};
-int Right[24] = {13, 18, 5, 15, 2, 7, 9, 14, -7, 11, -10, 3, -3, 10, -11, 7, -14, -9, -7, -2, -15, -5, -18, -13};
+static int Up[24] = {22, 15, 18, 7, 10, 3, 17, 10, 10, -5, 2, -9, 9, -2, 5, -10, -10, -17, -3, -10, -7, -18, -15, -22};
+static int Down[24] = {17, 22, 9, 18, 5, 10, 10, 15, -3, 10, -7, 2, -2, 7, -10, 3, -15, -10, -10, -5, -18, -9, -22, -17};
+static int Left[24] = {10, 7, 14, 11, 18, 15, -2, -5, 9, 3, 13, 7, -7, -13, -3, -9, 5, 2, -15, -18, -11, -14, -7, -10};
+static int Right[24] = {13, 18, 5, 15, 2, 7, 9, 14, -7, 11, -10, 3, -3, 10, -11, 7, -14, -9, -7, -2, -15, -5, -18, -13};
 
-int Pow24[7] = {1, 24, 576, 13824, 331776, 7962624, 191102976}; //, 4586471424L, 110075314176L, 2641807540224L;
+static int Pow24[7] = {1, 24, 576, 13824, 331776, 7962624, 191102976}; //, 4586471424L, 110075314176L, 2641807540224L;
 
-void n2tuple(int k, vector<int>& ret)
+void n2tuple(int k, std::vector<int>& ret)
 {
-  for (int i = 0; i < M*N; ++i) {
-    ret[i] = (k % 24);
-    k = k / 24;
-  }
+  for (int i : ret)
+    {
+      i = (k % 24);
+      k = k / 24;
+    }
 }
 
 int main()
 {
-  vector<bool> known(NSTATES, false);
+  std::vector<bool> known(NSTATES, false);
   known[0] = true;
 
-  hash_set<int> newKnown, prevLevel;
+  std::unordered_set<int> newKnown, prevLevel;
   newKnown.insert(0);
 
   int nNewKnown = 1;
   int nKnown = 1;
   int level = 0;
 
-  vector<int> tuple(M*N);
+  std::vector<int> tuple(M*N);
 
   while (0 != nNewKnown) {
-    cout << "There are " << (nNewKnown) << " states on level " << level << " .\n";
-    ++level;
-    int j, k, neighb;
-    prevLevel = newKnown;
-    newKnown.clear();
-    hash_set<int>::const_iterator it;
-    for (it = prevLevel.begin(); it != prevLevel.end(); ++it) {
-      // Convert to tuple
-      n2tuple(*it, tuple);
-	
-      // For all its neighbours, make them known, too.
-      // For each row
-      for (j = 0; j < M; ++j) {
-	// Up neighbour
-	neighb = *it;
-	for (k = j*N; k < (j+1)*N; ++k) {
-	  neighb += Up[tuple[k]] * Pow24[k];
-	}
-	if (!known[neighb]) {
-	  newKnown.insert(neighb);
-	}
-	// Down neighbour
-	neighb = *it;
-	for (k = j*N; k < (j+1)*N; ++k) {
-	  neighb += Down[tuple[k]] * Pow24[k];
-	}
-	if (!known[neighb]) {
-	  newKnown.insert(neighb);
-	}	}
-      // For each column
-      for (j = 0; j < N; ++j) {
-	// Left neighbour
-	neighb = *it;
-	for (k = j; k < M*N; k += N) {
-	  neighb += Left[tuple[k]] * Pow24[k];
-	}
-	if (!known[neighb]) {
-	  newKnown.insert(neighb);
-	}	  // Right neighbour
-	neighb = *it;
-	for (k = j; k < M*N; k += N) {
-	  neighb += Right[tuple[k]] * Pow24[k];
-	}
-	if (!known[neighb]) {
-	  newKnown.insert(neighb);
-	}
-      }
+      std::cout << "There are " << (nNewKnown) << " states on level " << level << " .\n";
+      ++level;
+      int j,k,neighb;
+      prevLevel = newKnown;
+      newKnown.clear();
+      for (auto i : prevLevel)
+        {
+          // Convert to tuple
+          n2tuple(i, tuple);
+
+          // For all its neighbours, make them known, too.
+          // For each row
+          for (j = 0; j < M; ++j) {
+              // Up neighbour
+              neighb = i;
+              for (k = j*N; k < (j+1)*N; ++k) {
+                  neighb += Up[tuple[k]] * Pow24[k];
+                  std::cout << "Up:" << neighb << std::endl;
+                }
+              if (!known[neighb]) {
+                  newKnown.insert(neighb);
+                }
+              // Down neighbour
+              neighb = i;
+              for (k = j*N; k < (j+1)*N; ++k) {
+                  neighb += Down[tuple[k]] * Pow24[k];
+                }
+              if (!known[neighb]) {
+                  newKnown.insert(neighb);
+                }	}
+          // For each column
+          for (j = 0; j < N; ++j) {
+              // Left neighbour
+              neighb = i;
+              for (k = j; k < M*N; k += N) {
+                  neighb += Left[tuple[k]] * Pow24[k];
+                }
+              if (!known[neighb]) {
+                  newKnown.insert(neighb);
+                }	  // Right neighbour
+              neighb = i;
+              for (k = j; k < M*N; k += N) {
+                  neighb += Right[tuple[k]] * Pow24[k];
+                }
+              if (!known[neighb]) {
+                  newKnown.insert(neighb);
+                }
+            }
+        }
+
+      for (auto i : newKnown)
+        {
+          known[i] = true;
+        }
+      nNewKnown = newKnown.size();
+      nKnown += nNewKnown;
     }
 
-    for (it = newKnown.begin(); it != newKnown.end(); ++it) {
-      known[*it] = true;
-    }
-    nNewKnown = newKnown.size();
-    nKnown += nNewKnown;
-  }
-
-  cout << "Altogether " << nKnown << " states reached.\n";
+  std::cout << "Altogether " << nKnown << " states reached.\n";
 }
--- oldJunk/search01.cpp
+++ oldJunk/search01.cpp
@@ -1,173 +1,164 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <future>
+#include <cmath>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <functional>
+#include <system_error>
 
-#define M 6
-#define N 1
-int NSTATES = 24*24*24*24*24*24;// Should be 24^(N*M)
-using namespace std;
+static const int M = 6;
+static const int N = 1;
+static unsigned long long NSTATES = 191102976;//4586471424L; //should be 24^(M*N)
 
 // Global arrays
-int Up[24] = {22, 15, 18, 7, 10, 3, 17, 10, 10, -5, 2, -9, 9, -2, 5, -10, -10, -17, -3, -10, -7, -18, -15, -22};
-int Down[24] = {17, 22, 9, 18, 5, 10, 10, 15, -3, 10, -7, 2, -2, 7, -10, 3, -15, -10, -10, -5, -18, -9, -22, -17};
-int Left[24] = {10, 7, 14, 11, 18, 15, -2, -5, 9, 3, 13, 7, -7, -13, -3, -9, 5, 2, -15, -18, -11, -14, -7, -10};
-int Right[24] = {13, 18, 5, 15, 2, 7, 9, 14, -7, 11, -10, 3, -3, 10, -11, 7, -14, -9, -7, -2, -15, -5, -18, -13};
+static int Up[24] = {22, 15, 18, 7, 10, 3, 17, 10, 10, -5, 2, -9, 9, -2, 5, -10, -10, -17, -3, -10, -7, -18, -15, -22};
+static int Down[24] = {17, 22, 9, 18, 5, 10, 10, 15, -3, 10, -7, 2, -2, 7, -10, 3, -15, -10, -10, -5, -18, -9, -22, -17};
+static int Left[24] = {10, 7, 14, 11, 18, 15, -2, -5, 9, 3, 13, 7, -7, -13, -3, -9, 5, 2, -15, -18, -11, -14, -7, -10};
+static int Right[24] = {13, 18, 5, 15, 2, 7, 9, 14, -7, 11, -10, 3, -3, 10, -11, 7, -14, -9, -7, -2, -15, -5, -18, -13};
 
-int Pow24[7] = {1, 24, 576, 13824, 331776, 7962624, 191102976}; //, 4586471424L, 110075314176L, 2641807540224L;
+static int Pow24[7] = {1, 24, 576, 13824, 331776, 7962624, 191102976}; //, 4586471424L, 110075314176L, 2641807540224L;
+//would try with atomics only
+static std::vector<bool> known(NSTATES,false);
+static std::vector<bool> newKnown(NSTATES,false);
+static std::atomic<int> nNewKnown(0);
 
-vector<bool> known(NSTATES, false);
-vector<bool> oldKnown(NSTATES, false);
-vector<bool> newKnown(NSTATES, false);
-vector<int> myTuple(M*N);
-atomic<int> nNewKnown;
-
-void n2myTuple(int k, vector<int>& ret)
+void n2tuple(unsigned long long k, std::vector<int>& ret)
 {
-	for (int i = 0; i < M*N; ++i) 
-	{
-		ret[i] = (k % 24);
-		k = k / 24;
-	}
+  for (int i = 0; i < M*N; ++i) {
+    ret[i] = (k % 24);
+    k = k / 24;
+  }
 }
-
-void doit(int start,int end)
+void worker(unsigned long long rangeStart,
+            unsigned long long rangeEnd)
 {
-	int curr, j, k, neighb;
-	for (curr = start; curr < end; ++curr) 
-	{
-		// For all known points
-		if (oldKnown[curr]) 
-		{
-			// Convert to myTuple
-			n2myTuple(curr, myTuple);
+  std::vector<int> tuple(M*N);
+  unsigned long long curr, j, k, neighb;
+  for (curr = rangeStart; curr < rangeEnd; ++curr) {
+    // For all known points
+    if (known[curr]) {
+      // Convert to tuple
+      n2tuple(curr, tuple);
+      std::cout << "doing work\n";
 
-			// For all its neighbours, make them known, too.
-			// For each row
-			for (j = 0; j < M; ++j) 
-			{
-				// Up neighbour
-				neighb = curr;
-				for (k = j*N; k < (j+1)*N; ++k) 
-				{
-					neighb += Up[myTuple[k]] * Pow24[k];
-				}
-				{
-					newKnown[neighb] = true;
-				}
-				// Down neighbour
-				neighb = curr;
-				for (k = j*N; k < (j+1)*N; ++k) 
-				{
-					neighb += Down[myTuple[k]] * Pow24[k];
-				}
-				{
-					newKnown[neighb] = true;
-				}
-			}
-			// For each column
-			for (j = 0; j < N; ++j) 
-			{
-				// Left neighbour
-				neighb = curr;
-				for (k = j; k < M*N; k += N) 
-				{
-					neighb += Left[myTuple[k]] * Pow24[k];
-				}
-				{
-					newKnown[neighb] = true;
-				}
-				// Right neighbour
-				neighb = curr;
-				for (k = j; k < M*N; k += N) 
-				{
-					neighb += Right[myTuple[k]] * Pow24[k];
-				}
-				{
-					newKnown[neighb] = true;
-				}
-			}
-		}
-	}
+      // For all its neighbours, make them known, too.
+      // For each row
+      for (j = 0; j < M; ++j) {
+        // Up neighbour
+        neighb = curr;
+        for (k = j*N; k < (j+1)*N; ++k) {
+          neighb += Up[tuple[k]] * Pow24[k];
+        }
+        if (!newKnown[neighb]) {
+          ++nNewKnown;
+          newKnown[neighb] = true;
+        }
+        // Down neighbour
+        neighb = curr;
+        for (k = j*N; k < (j+1)*N; ++k) {
+          neighb += Down[tuple[k]] * Pow24[k];
+        }
+        if (!newKnown[neighb]) {
+          ++nNewKnown;
+          newKnown[neighb] = true;
+        }
+      }
+      // For each column
+      for (j = 0; j < N; ++j) {
+        // Left neighbour
+        neighb = curr;
+        for (k = j; k < M*N; k += N) {
+          neighb += Left[tuple[k]] * Pow24[k];
+        }
+        if (!newKnown[neighb]) {
+          ++nNewKnown;
+          newKnown[neighb] = true;
+        }
+        // Right neighbour
+        neighb = curr;
+        for (k = j; k < M*N; k += N) {
+          neighb += Right[tuple[k]] * Pow24[k];
+        }
+        if (!newKnown[neighb]) {
+          ++nNewKnown;
+          newKnown[neighb] = true;
+        }
+      }
+    }
+  }
+  for (int i = 0; i < NSTATES; ++i) {
+    if (newKnown[i] && !known[i]) {
+      known[i] = true;
+    }
+  }
 }
-
-void doit2(int begin,int end)
+std::vector<bool> merger(std::vector<std::vector<bool> > knowns)
 {
-	int myNewKnown = 0;
-	for (int i = begin; i < end; ++i) 
-	{
-		if (newKnown[i] && !known[i]) 
-		{
-			oldKnown[i] = true;
-			known[i] = true;
-			++myNewKnown;
-		} else
-		{
-			oldKnown[i] = false;
-		}
-	}
-	nNewKnown += myNewKnown;
+  std::vector<bool> ret(NSTATES,false);
+  for(size_t i = 0; i < knowns.size();++i)
+    {
+      for (unsigned long long j = 0;j < NSTATES;++j)
+        {
+          ret[j] = ret[j] || knowns[i][j];
+        }
+    }
+  return ret;
 }
 
 int main()
 {
-	int THREADS = std::thread::hardware_concurrency();
-	cout <<"The program will use "<<THREADS << " concurent threads!"<<endl;
-	known[0] = true;
-	oldKnown[0] = true;
+  auto start = std::chrono::system_clock::now();
 
-	int nKnown = 0;
-	nNewKnown = 1;
-	int level = 0;
-	auto start = std::chrono::steady_clock::now();
-	
-	while (nKnown != nNewKnown) 
-	{
-		newKnown = oldKnown;
-		cout << "There are " << (nNewKnown - nKnown) << " states on level " << level << " .\n";
-		++level;
-		nKnown = nNewKnown;
-		
-		if(THREADS > 1)
-		{
-			//concurrent threads
-			std::vector<thread> results;
-			for (size_t i = 0; i < THREADS-1; i++)
-			{
-				results.push_back(thread(doit,i*(NSTATES/THREADS),(i+1)*(NSTATES/THREADS) ));
-			}
-			results.push_back(thread(doit,(THREADS-1)*(NSTATES/THREADS),NSTATES ));
+  //threads init
+  unsigned int thread_count = std::thread::hardware_concurrency();
+  std::cout << "I will run on " << thread_count << " threads\n";
+  std::vector<std::thread> workers(thread_count);
 
-			for (size_t i = 0; i < THREADS; i++)
-			{
-				results[i].join();
-			}
-		} else {
-			doit(0,NSTATES);
-		}
-		
-		if(THREADS > 1)
-		{
-			//concurrent threads
-			std::vector<thread> results;
-			for (size_t i = 0; i < THREADS-1; i++)
-			{
-				results.push_back(thread(doit2,i*(NSTATES/THREADS),(i+1)*(NSTATES/THREADS) ));
-			}
-			results.push_back(thread(doit2,(THREADS-1)*(NSTATES/THREADS),NSTATES ));
+  //graph init
+//  std::vector<bool> known(NSTATES,false);
+//  std::vector<std::vector<bool>> knowns(thread_count,std::vector<bool>(NSTATES,false));
+//  for (auto vec : knowns)
+//    vec[0] = true;
 
-			for (size_t i = 0; i < THREADS; i++)
-			{
-				results[i].join();
-			}
-		} else {
-			doit2(0,NSTATES);
-		}
-		cout << endl;
+//  std::vector<bool> newKnown(NSTATES,false);
+//  std::vector<std::vector<bool>> newKnowns(thread_count,std::vector<bool>(NSTATES,false));
+//  for (auto vec : newKnowns)
+//    vec[0] = true;
 
-	}
-	auto end = std::chrono::steady_clock::now();
-	cout << "Altogether " << nKnown << " states reached.\n";
-	std::chrono::duration<double> diff = end-start;
-	std::cout << "Time required:" << diff.count() << " s\n";
+  int nKnown = 0;
+  std::atomic<int> nNewKnown(1);
+  int level = 0;
+  while (nKnown != nNewKnown) {
+    std::cout << "There are " << (nNewKnown - nKnown) << " states on level " << level << " .\n";
+    ++level;
+    nKnown = nNewKnown;
+    //copy data
+//    for(auto vec : knowns)
+//        std::copy(known.begin(),known.end(),vec.begin());
+
+//    for(auto vec : newKnowns)
+//        std::copy(newKnown.begin(),newKnown.end(),vec.begin());
+
+    //start threads
+    for (size_t i = 0; i < thread_count -1;++i)
+      {
+          workers.push_back(std::thread(worker,
+                                        i*(NSTATES/thread_count),
+                                        (i+1)*(NSTATES/thread_count)));
+      }
+    workers.push_back(std::thread(worker,
+                                  (thread_count-1)*(NSTATES/thread_count),
+                                  NSTATES));
+    for(auto& t : workers)
+      t.join();
+    std::cout << std::endl;
+
+  }
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+
+  std::cout << "Altogether " << nKnown << " states reached.\n";
+  std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
 }
